7b17a91f8dbe11135e450438ec878e6914070402
Georgios Kafanas <g.kafanas@greeklug.gr>
2023-11-23 00:00:27 +0100

    Initialize a demo for the matrix market I/O library

---
+++ src/read_matrix.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mmio.h"
+
+int main( int argc, char** argv )
+{
+  if ( argc != 2 ) {
+    printf("Usage: %s <matrix file name>\n", argv[0]);
+    return EXIT_SUCCESS;
+  }
+
+  FILE* f = (FILE*) fopen(argv[1], "r");
+  if ( f == NULL ) {
+    fprintf(stderr, "fopen() failed in file %s at line # %d", __FILE__, __LINE__);
+    return EXIT_FAILURE;
+  }
+
+  MM_typecode type;
+  int res = mm_read_banner(f, &type);
+  if ( res ) {
+    fprintf(stderr, "Failed to read the matrix banner in file %s at line # %d", __FILE__, __LINE__);
+    return EXIT_FAILURE;
+  }
+
+  int M, N, nz;
+  res = mm_read_mtx_crd_size(f, &M, &N, &nz);
+  if ( res ) {
+    fprintf(stderr, "Failed to read the matrix size data in file %s at line # %d", __FILE__, __LINE__);
+    return EXIT_FAILURE;
+  }
+
+  int* I = (int*) malloc( nz*sizeof(int) );
+  int* J = (int*) malloc( nz*sizeof(int) );
+  double* val = (double*) malloc( nz*sizeof(double) );
+
+  res = mm_read_mtx_crd_data(f, type,
+    M, N, nz,
+    I, J, val);
+  if ( res ) {
+    fprintf(stderr, "Failed to read the matrix data in file %s at line # %d", __FILE__, __LINE__);
+    return EXIT_FAILURE;
+  }
+
+  res = fclose(f);
+  if ( res ) {
+    fprintf(stderr, "Failed to close the file (see 'errno' more details) in file %s at line # %d", __FILE__, __LINE__);
+    return EXIT_FAILURE;
+  }
+
+  for ( int i = 0; i < nz; ++i ) {
+    printf ("%d: (%d , %d) = %lg\n", i, I[i], J[i], val[i] );
+  }
+
+  return EXIT_SUCCESS;
+}
