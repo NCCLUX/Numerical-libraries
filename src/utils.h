5580d9ae29eea693fc85a926ba08a7547767159f
Georgios Kafanas <g.kafanas@greeklug.gr>
2025-02-13 22:28:20 +0100

    [STYLE] Fix style issues in interface definitions

--- src/utils.h
+++ src/utils.h
@@ -25,11 +25,14 @@ typedef struct _matrix matrix;
 
 void sample_harmonic_signal(
   double const alpha, double const omega, double const phi,
-  int const n, double* const v );
+  int const n, double* const v
+);
 
-void copy_coo_to_dns( int const nz, int const ld, int const ncols,
+void copy_coo_to_dns(
+  int const nz, int const ld, int const ncols,
   int const* const row, int const* const col, double const* const a,
-  double* const val );
+  double* const val
+);
 
 int get_ld( int const entries, int const type_cache_alignment );
 
@@ -37,8 +40,12 @@ void print_matrix_struct( FILE* const f, matrix const* const a );
 
 void print_vector_struct( FILE* const f, vector const* const v );
 
-void print_matrix( FILE* const f,
-  int const ld, int const nrows, int const ncols, double const* const a );
+void print_matrix(
+  FILE* const f,
+  int const ld, int const nrows, int const ncols, double const* const a
+);
 
-void print_vector( FILE* const f,
-  int const nrows, int const inc, double const* const v );
+void print_vector(
+  FILE* const f,
+  int const nrows, int const inc, double const* const v
+);
